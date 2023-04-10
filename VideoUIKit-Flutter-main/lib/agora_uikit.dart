9001965152c87a806c99511c5377d0d947d9e182
Navin-Agarwal <navinagarwal0420@gmail.com>
2023-04-10 12:50:39 +0530

    Video UI

---
+++ VideoUIKit-Flutter-main/lib/agora_uikit.dart
@@ -0,0 +1,67 @@
+export 'package:agora_rtc_engine/agora_rtc_engine.dart'
+    show
+        VideoEncoderConfiguration,
+        ChannelProfileType,
+        ClientRoleType,
+        StreamFallbackOptions,
+        AudioProfileType,
+        AudioScenarioType,
+        BeautyOptions,
+        LighteningContrastLevel,
+        ErrorCodeType,
+        RtcStats,
+        UserOfflineReasonType,
+        RemoteVideoState,
+        RemoteVideoStateReason,
+        RemoteAudioState,
+        RemoteAudioStateReason,
+        LocalAudioStreamState,
+        LocalAudioStreamError,
+        AudioVolumeInfo,
+        LocalVideoStreamState,
+        LocalVideoStreamError,
+        AreaCode,
+        UserInfo,
+        ConnectionStateType,
+        ConnectionChangedReasonType,
+        NetworkType,
+        Rectangle,
+        LastmileProbeResult,
+        LocalVideoStats,
+        LocalAudioStats,
+        RemoteVideoStats,
+        RemoteAudioStats,
+        AudioMixingStateType,
+        AudioMixingReasonType,
+        RtmpStreamPublishState,
+        RtmpStreamPublishErrorType,
+        InjectStreamStatus,
+        ChannelMediaRelayState,
+        ChannelMediaRelayError,
+        ChannelMediaRelayEvent,
+        StreamPublishState,
+        StreamSubscribeState,
+        RtmpStreamingEvent,
+        UploadErrorReason;
+export 'package:agora_rtm/agora_rtm.dart'
+    show
+        AgoraRtmMessage,
+        AgoraRtmMember,
+        AgoraRtmChannelAttribute,
+        AgoraRtmChannelException,
+        AgoraRtmClientException,
+        AgoraRtmLocalInvitation,
+        AgoraRtmRemoteInvitation;
+export 'package:permission_handler/permission_handler.dart';
+
+export 'models/agora_channel_data.dart' show AgoraChannelData;
+export 'models/agora_connection_data.dart' show AgoraConnectionData;
+export 'models/agora_rtc_event_handlers.dart' show AgoraRtcEventHandlers;
+export 'models/agora_rtm_client_event_handler.dart'
+    show AgoraRtmClientEventHandler;
+export 'models/agora_rtm_channel_event_handler.dart'
+    show AgoraRtmChannelEventHandler;
+export 'src/agora_client.dart' show AgoraClient;
+export 'src/buttons/buttons.dart' show AgoraVideoButtons;
+export 'src/enums.dart';
+export 'src/layout/layout.dart' show AgoraVideoViewer;
